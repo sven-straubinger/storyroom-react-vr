6b360dfb96260df21904b6ea821638134f7ed17a
Sven Straubinger <straubinger.es@googlemail.com>
2018-03-13 23:40:59 +0100

    Add deer

--- index.vr.js
+++ index.vr.js
@@ -6,6 +6,7 @@ import {
   Text,
   Image,
   View,
+  Model
 } from 'react-vr';
 
 export default class Storyroom extends React.Component {
@@ -18,8 +19,19 @@ export default class Storyroom extends React.Component {
             width: 1,
             height: 1,
             layoutOrigin: [0.5, 0.5],
-            transform: [{translate: [0, 0, -5]}],
+            transform: [{translate: [0.08, 0, -12]}],
           }}
+        />
+        <Model
+          source={{
+            obj: asset("Deer.obj")
+          }}
+          style={{
+            transform: [{translate: [1.5, -1, 3]}, {rotateY : 160}]
+          }}
+          texture={
+            asset("Diffuse.jpg")
+          }
         />
       </View>
     );
